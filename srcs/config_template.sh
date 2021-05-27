bab243cde99dbf0d6faa6a3406c649a754899349
Jules Rivoire <julesriv@gmail.com>
2021-05-27 16:01:23 +0200

    Adding grading scale to config template

--- srcs/config_template.sh
+++ srcs/config_template.sh
@@ -24,8 +24,20 @@ PATH_TO_PUSH_SWAP=REPLACE_THIS_WITH_THE_CORRECT_PATH
 
 #By default, the max number of actions is set to score 4 points for stacks of size 100 and 500.
 #Edit the next line to change the scale for 100 numbers.
+#Less than | Points
+#   700    |  5
+#   900    |  4
+#   1100   |  3
+#   1300   |  2
+#   1500   |  1
 one_hundred_max=900
 #Edit the next line to change the scale for 500 numbers.
+#Less than | Points
+#   5500   |  5
+#   7000   |  4
+#   8500   |  3
+#   10000  |  2
+#   11500  |  1
 five_hundred_max=7000
 #By default, the average will be taken over 25 runs of the function.
 #Edit the next line to change the number of runs over which the average is taken.
